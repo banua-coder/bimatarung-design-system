12f8488a01f4193c4b8255995eb03841f9af2b81
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-05-10 14:10:43 +0800

    feat(file_picker): revert changes to file picker

--- lib/src/components/card/bt_file_picker_card.dart
+++ lib/src/components/card/bt_file_picker_card.dart
@@ -1,10 +1,15 @@
+/// @author Fajrian Aidil Pratama
+/// @email fajrianaidilp@gmail.com
+/// @create date 2023-05-07 16:25:58
+/// @modify date 2023-05-07 16:25:58
+/// @desc
+
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 import '../../../bimatarung_design_system.dart';
-import 'enum/bt_file_picker_state.dart';
 
-class BTFilePickerCard extends StatefulWidget {
+class BTFilePickerCard extends StatelessWidget {
   const BTFilePickerCard({
     super.key,
     this.fileName,
@@ -12,144 +17,94 @@ class BTFilePickerCard extends StatefulWidget {
     this.onSelect,
     this.onUpdate,
     this.onDelete,
-    this.errorMessage = 'Tidak ada file yang dipilih!',
     this.placeholder = 'Pilih file untuk diunggah!',
   });
 
   final String? fileName;
   final String? fileSize;
   final String placeholder;
-  final String errorMessage;
-  final Future Function()? onSelect;
-  final Future Function()? onUpdate;
-  final Future Function()? onDelete;
-
-  @override
-  State<BTFilePickerCard> createState() => _BTFilePickerCardState();
-}
-
-class _BTFilePickerCardState extends State<BTFilePickerCard> {
-  final ValueNotifier<BTFilePickerState> _state =
-      ValueNotifier(BTFilePickerState.idle);
-  final ValueNotifier<bool> _isTouched = ValueNotifier(false);
-
-  @override
-  void initState() {
-    _checkState();
-    super.initState();
-  }
-
-  void _checkState() {
-    _state.value = !_isTouched.value
-        ? widget.fileName == null && widget.fileSize == null
-            ? BTFilePickerState.idle
-            : BTFilePickerState.valid
-        : widget.fileName == null && widget.fileSize == null
-            ? BTFilePickerState.invalid
-            : BTFilePickerState.valid;
-
-    setState(() {});
-  }
+  final GestureTapCallback? onSelect;
+  final GestureTapCallback? onUpdate;
+  final GestureTapCallback? onDelete;
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: _isTouched,
-      builder: (_, __, ___) => ValueListenableBuilder(
-        valueListenable: _state,
-        builder: (_, value, __) => DottedBorder(
-          borderType: BorderType.RRect,
-          color: value.style.borderColor,
-          radius: Radius.circular(8.r),
-          child: Material(
-            color: value.style.bgColor,
-            borderRadius: BorderRadius.circular(8.r),
-            child: InkWell(
-              onTap: () async {
-                widget.fileName != null && widget.fileSize != null
-                    ? await widget.onUpdate?.call()
-                    : await widget.onSelect?.call();
-                _checkState();
-              },
-              onTapUp: (details) {
-                _isTouched.value = true;
-                _checkState();
-              },
-              splashColor: value.style.splashColor,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 16.w,
-                  vertical: 8.h,
-                ),
-                child: value == BTFilePickerState.idle ||
-                        value == BTFilePickerState.invalid
-                    ? Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(
-                            child: Icon(
-                              Icons.upload_file_rounded,
-                              color: value.style.foregroundColor,
-                            ),
-                          ),
-                          8.verticalSpace,
-                          Text(
-                            value == BTFilePickerState.invalid
-                                ? widget.errorMessage
-                                : widget.placeholder,
-                            style: BTTextStyle.bodySmall(
-                              color: value.style.foregroundColor,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      )
-                    : Row(
-                        children: [
-                          Icon(
-                            Icons.file_present_rounded,
-                            color: value.style.foregroundColor,
-                          ),
-                          8.horizontalSpace,
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.fileName ?? '',
-                                  style: BTTextStyle.body(
-                                    fontWeight: FontWeight.w700,
-                                    color: value.style.foregroundColor,
-                                  ),
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                4.verticalSpace,
-                                Text(
-                                  widget.fileSize ?? '',
-                                  style: BTTextStyle.caption(
-                                    fontWeight: FontWeight.w500,
-                                    color: value.style.foregroundColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          8.horizontalSpace,
-                          IconButton(
-                            onPressed: () async {
-                              await widget.onDelete?.call();
-                              _checkState();
-                            },
-                            icon: const Icon(
-                              Icons.delete,
-                              color: BTColors.error,
-                            ),
-                          ),
-                        ],
-                      ),
-              ),
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      color: BTColors.primary,
+      radius: Radius.circular(8.r),
+      child: Material(
+        color: BTColors.primaryLighter,
+        borderRadius: BorderRadius.circular(8.r),
+        child: InkWell(
+          onTap: fileName != null && fileSize != null ? onUpdate : onSelect,
+          splashColor: BTColors.primaryDarker.withOpacity(0.3),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.w,
+              vertical: 8.h,
             ),
+            child: fileName == null && fileSize == null
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Center(
+                        child: Icon(
+                          Icons.upload_file_rounded,
+                          color: BTColors.primaryDarker,
+                        ),
+                      ),
+                      8.verticalSpace,
+                      Text(
+                        placeholder,
+                        style: BTTextStyle.bodySmall(
+                          color: BTColors.primary,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  )
+                : Row(
+                    children: [
+                      const Icon(
+                        Icons.file_present_rounded,
+                        color: BTColors.primaryDarker,
+                      ),
+                      8.horizontalSpace,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              fileName ?? '',
+                              style: BTTextStyle.body(
+                                fontWeight: FontWeight.w700,
+                                color: BTColors.primaryDarker,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            4.verticalSpace,
+                            Text(
+                              fileSize ?? '',
+                              style: BTTextStyle.caption(
+                                fontWeight: FontWeight.w500,
+                                color: BTColors.primary,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      8.horizontalSpace,
+                      IconButton(
+                        onPressed: onDelete,
+                        icon: const Icon(
+                          Icons.delete,
+                          color: BTColors.error,
+                        ),
+                      ),
+                    ],
+                  ),
           ),
         ),
       ),
